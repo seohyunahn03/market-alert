1aa8440023b67f306dc4efe844c071b8766b8cd1
seohyunahn <taehwanahn0@gmail.com>
2026-04-30 15:05:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import yfinance as yf
 import requests
+import fear_greed
 from datetime import datetime
 import pytz
 
-# 🚨 여기에 본인이 만든 채널명을 꼭 다시 적어주세요!
-NTFY_CHANNEL = "taehwan_vix_alert_2026" 
+# 🚨 본인이 만든 ntfy 채널명으로 반드시 변경해주세요! (예: taehwan_vix_alert_2026)
+NTFY_CHANNEL = " taehwan_vix_alert_2026" 
 
 def get_market_data():
+    # VIX 지수 가져오기
     vix = yf.Ticker("^VIX").history(period="1d")['Close'].iloc[-1]
-    url = "https://production.dataviz.cnn.io/index/fearandgreed/graphdata"
     
-    # 봇(Bot) 차단을 막기 위해 일반 브라우저처럼 위장하는 부분 (수정 완료!)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-        'Accept': 'application/json, text/plain, */*',
-        'Referer': 'https://edition.cnn.com/'
-    }
+    # CNN 차단 우회 전용 라이브러리 사용
+    fng_data = fear_greed.get()
+    fng_score = fng_data['score']
+    fng_rating = fng_data['rating']
     
-    response = requests.get(url, headers=headers).json()
-    fng_score = response['fear_and_greed']['score']
-    fng_rating = response['fear_and_greed']['rating']
     return round(vix, 2), round(fng_score), fng_rating
 
 def send_push(title, message, priority="default", tags="chart_with_upwards_trend"):
